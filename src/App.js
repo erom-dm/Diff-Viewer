f447d26817e5ef32397a85b4744add2c8869ec6b
erom-dm <swiftyerom@gmail.com>
2019-03-02 20:29:00 +0200

    working TreeView prototype

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import {Container} from 'semantic-ui-react';
+import ListExampleTree from './components/List';
+import TreeView from './components/treeview';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        hey there!
-      </div>
+        <Fragment>
+            <Container>
+                <div className="App">
+                    <ListExampleTree/>
+                </div>
+            </Container>
+            <div>
+                <TreeView/>
+            </div>
+        </Fragment>
     );
   }
 }
